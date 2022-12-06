fcf76686c0512c5b64ac3365ea6cd41ffb84465b
raminatastanova <raminatastanova@gmail.com>
2022-12-06 21:53:23 +0100

    Completed Flash Chat App

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -2,21 +2,42 @@
 //  WelcomeViewController.swift
 //  Flash Chat iOS13
 //
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
+//  Created by Рамина Ергалиева on 21.11.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleLabel.text = K.appName
 
-       
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//        charIndex += 1
+//
+//        }  //how to make animation without cocoapod
     }
-    
-
 }
+
+
---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Рамина Ергалиева on 21.11.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String  //sender email
+    let body: String 
+}
